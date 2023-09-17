1e3408797d730eea95a96daf3fa8ab5863f01518
Pivkina <ketrin_696@mail.ru>
2023-09-17 12:31:26 +0200

    "исправления1" commit

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,5 +1,6 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.web.data.DataHelper;
@@ -12,39 +13,31 @@ import static com.codeborne.selenide.Selenide.$$;
 public class DashboardPage {
 
 
-  private SelenideElement heading = $("[data-test-id=dashboard]");
+  private final SelenideElement heading = $("[data-test-id=dashboard]");
   private final String balanceStart = "баланс: ";
   private final String balanceFinish = " р.";
-  private ElementsCollection cards = $$(".list__item div");
+  private final ElementsCollection cards = $$(".list__item div");
 
   public DashboardPage() {
     heading.shouldBe(visible);
   }
 
-  // public int getCardBalance(DataHelper.CardInfo cardInfo) {
-  //var text = cards.findBy(text(cardInfo.getCardNumber().substring(15))).getText();
-  // return extractBalance(text);
- //} тут
+  public int getCardBalance(DataHelper.CardInfo cardInfo) {
+  var text = cards.findBy(Condition.text(cardInfo.getCardNumber().substring(15))).getText();
+  return extractBalance(text);
+ }
 
 
-  public int getCardBalance(int CardInfo) {
-   var text = cards.get(CardInfo).getText();
-   return extractBalance(text);
-  }
+ // public int getCardBalance(int CardInfo) {
+ ///  var text = cards.get(CardInfo).getText();
+ ///  return extractBalance(text);
+//  }
 
   public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
     cards.findBy(attribute("data-test-id", cardInfo.getTestId())).$("button").click();
     return new TransferPage();
   }
 
-
-
-  // private int extractBalance(String text) {
-   // var start = text.indexOf(balanceStart);
-  //  var finish = text.indexOf(balanceFinish);
-    //var value = text.substring(start + balanceStart.length(), finish);
-  //  return Integer.parseInt(value);
- // }
 
   private int extractBalance(String text) {
     var start = text.indexOf(balanceStart);
